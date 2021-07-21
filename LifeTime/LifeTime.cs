9195cf10e0b5d771fde1d26e3305b1020c8d6f30
Tomohito Nakayama <tmht@7k8m.me>
2021-07-21 22:09:20 +0900

    Add project files.

---
+++ LifeTime/LifeTime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LifeTime
+{
+    public abstract class LifeTime<T> where T:IDisposable
+    {
+        public delegate T FactoryMethod();
+        private FactoryMethod _createInstance;
+
+        public delegate void Destiny(T obj);
+
+        public LifeTime(FactoryMethod createInstance)
+        {
+            _createInstance = createInstance;
+        }
+
+        public void LiveThrough(Destiny d)
+        {
+            using(T obj = _createInstance())
+            {
+                d(obj);
+            }
+        } 
+    }
+}
